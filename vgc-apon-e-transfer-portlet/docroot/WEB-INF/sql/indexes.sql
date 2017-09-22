21e0f457dc728a2f1ec3432d66f346fae2ed3302
bkk <qq12352100@163.com>
2017-09-22 10:55:56 +0800

    clear

--- vgc-apon-e-transfer-portlet/docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,5 +0,0 @@
-create index IX_45A70197 on vgcapon_ETransfer (status, userId);
-
-create index IX_FDAB867C on vgcapon_HeadcountCI (status, userId);
-
-create index IX_EBB288CF on vgcapon_HeadcountLE (status, userId);

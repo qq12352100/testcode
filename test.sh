cecf171df71416f18d7a9735161f9da1d4882310
bkk <qq12352100@163.com>
2017-09-22 11:25:04 +0800

    $1

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "remove classes file!"
+rm -rf vgc-apon-e-closing-portlet/docroot/WEB-INF/classes
+rm -rf vgc-apon-e-onboarding-portlet/docroot/WEB-INF/classes
+rm -rf vgc-apon-e-transfer-portlet/docroot/WEB-INF/classes
+rm -rf vgc-apon-seat-office-portlet/docroot/WEB-INF/classes
+sleep 1s
+git add .
+sleep 1s
+git commit -m '$1'
+sleep 1s
+git push origin master
+sleep 1s
+echo "remove all file!"
+rm -rf vgc-apon*
+sleep 3s
+echo "ok!"

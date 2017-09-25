c9723447f87093201094b95382a56e8e96035d02
bkk <qq12352100@163.com>
2017-09-25 17:22:05 +0800

    audi2

--- test.sh
+++ test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 echo "remove classes file!"
-rm -rf vgc-apon-business-trip-portlet/docroot/WEB-INF/classes
-rm -rf vgc-apon-e-leave-portlet/docroot/WEB-INF/classes
+#声明字符串
+a="vgc-apon-business-trip-portlet,vgc-apon-e-leave-portlet"
+#分割字符串到数组
+OLD_IFS="$IFS" 
+IFS="," 
+arr=($a) 
+IFS="$OLD_IFS" 
+for s in ${arr[@]} 
+do 
+	rm -rf "$s"/docroot/WEB-INF/classes
+	rm -rf "$s"/docroot/WEB-INF/lib
+	rm -rf "$s"/docroot/WEB-INF/service
+	
+	#rm -rf "$s"/docroot/WEB-INF/src/com/business/trip/model/impl
+	
+	#find "$s"/docroot/WEB-INF/src/com -type d -name "model" 
+done
 sleep 1s
 git add .
 sleep 3s
